ca596b23657421b8b5cd7728d39693315650fc13
Kenny Kerr <kekerr@microsoft.com>
2018-01-08 09:38:15 -0800

    Samples

---
+++ DesktopComposition/pch.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <winrt/Windows.System.h>
+#include <winrt/Windows.UI.Composition.Desktop.h>
+#include <windows.ui.composition.interop.h>
+#include <ShellScalingAPI.h>
+#include <DispatcherQueue.h>
